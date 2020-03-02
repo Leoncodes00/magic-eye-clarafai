35c0049c5a56750309f5a060e515de6d6ea119d0
Leoncodes00 <chachaiyang3@gmail.com>
2020-03-02 10:07:44 -0800

    Updates

---
+++ precache-manifest.effd1c85b8c9936156fe1fe8fb4ac8ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f7ab14cbfe53cd03606a351d6e6d80",
+    "url": "/magic-eye-clarafai/index.html"
+  },
+  {
+    "revision": "5d44b2117a9fa3adf315",
+    "url": "/magic-eye-clarafai/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bdb73cb89e397e303c57",
+    "url": "/magic-eye-clarafai/static/css/main.34d1ffc9.chunk.css"
+  },
+  {
+    "revision": "5d44b2117a9fa3adf315",
+    "url": "/magic-eye-clarafai/static/js/2.8376c1d6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/magic-eye-clarafai/static/js/2.8376c1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb73cb89e397e303c57",
+    "url": "/magic-eye-clarafai/static/js/main.926dc734.chunk.js"
+  },
+  {
+    "revision": "04f20aa1d48cb462b8ea",
+    "url": "/magic-eye-clarafai/static/js/runtime-main.b07e899b.js"
+  },
+  {
+    "revision": "ab967ba029314d41c060c065da90e4f4",
+    "url": "/magic-eye-clarafai/static/media/london-eye.ab967ba0.png"
+  }
+]);
